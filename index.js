d495f9b08f8c282efd39a9265876303603507871
Cody Romano <codyromano@gmail.com>
2017-07-01 14:48:56 -0700

    Set up default route

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const config = require('./config');
 const ProxyServer = require('./server/ProxyServer');
+const setupDefaultRoute = require('./server/setupDefaultRoute');
 const { childAppManifest, httpPort} = config.getAll();
 
-const server = new ProxyServer();
+const server = new ProxyServer({
+  onBeforeListen: setupDefaultRoute
+});
 
 childAppManifest.forEach(childApp =>
   server.addProxyRoute(childApp)
